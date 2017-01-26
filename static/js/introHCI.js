c72b6f3d239b66896fb7c09bc2f7e20b85f245d1
Rui Jiang <juneruijiang@gmail.com>
2017-01-26 11:09:23 -0800

    add interactivities

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,23 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
+	$(".project").click(projectClick);
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e){
+	e.preventDefault();
+	var containingProject = $(e.target).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+}
+
